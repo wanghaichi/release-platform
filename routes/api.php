66da2a87f89afd2234448e5a08003d2b07bf5ea1
wanghaichi <703007993@qq.com>
2017-10-05 14:39:09 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::group(['prefix' => 'app', 'namespace' => 'Api'], function(){
+Route::group(['prefix' => 'app', 'namespace' => 'Api', 'middleware' => ['cors']], function(){
     Route::get('info/{id?}', 'AppController@info');
     Route::get('latest-version-code/{id}', "AppController@latestVersionCode");
 });
